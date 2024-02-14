ad00cc4979c76ed4643abf844695f0ab06bdfed4
Sabina777 <sabina.prajapati777@gmail.com>
2024-02-14 15:10:48 +0000

    Validation logics added in auth and admin controllers

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-'mongodb+srv://kumar:Black-6594@cluster0.b2emiit.mongodb.net/?retryWrites=true&w=majority'
-;
+  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
